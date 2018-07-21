a8f608e8410324daa323dab560dddf87be61650d
root <root@localhost.localdomain>
2018-07-21 16:54:03 -0700

    completed basic functions

--- network_scanner.py
+++ network_scanner.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python
 
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 import scapy.all as scapy
+import argparse
 
+
+# def scan(ip):
+#     scapy.arping(ip)
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-t", "--target", dest="target", help="A target or target range to scan")
+
+    options = parser.parse_args()
+
+    return options
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    clients_list = []
 
-scan("10.0.2.1/24")
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+
+    return clients_list
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
